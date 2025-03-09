1d996e2a3287bfb99de18a568467486be671fb08
Yevhenii Holubnychyi <y.holubnychyi@gmail.com>
2025-03-09 11:33:56 -0230

    Covers (#4)
    * COVERS_BETA1
    
    * COVERS_BETA2

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,1 +1,22 @@
-// Coming soon... 
+const coversList = document.querySelectorAll('li');
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null,
+  rootMargin: '-25% 0px -25% 0px',
+  threshold: 0,
+});
+const coversSection = document.querySelector('.covers-section');
+observer.observe(coversSection);
+
+function handleIntersection(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      coversList.forEach(item => {
+        item.style.animationDuration = '20s';
+      });
+    } else {
+      coversList.forEach(item => {
+        item.style.animationDuration = '0s';
+      });
+    }
+  });
+}
