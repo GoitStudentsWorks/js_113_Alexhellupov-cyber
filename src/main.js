647357fe1b0eb4cba1da1f53b3567917114c7ccb
AlexHellupov <alexhell132@gmail.com>
2025-03-09 13:22:27 +0300

    upd about-me

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/about-me-2';
+import './js/benefits';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
+import './js/colors';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollUpButton = document.getElementById('scrollUp');
+  const scrollThreshold = 300;
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > scrollThreshold) {
+      scrollUpButton.classList.remove('hidden');
+    } else {
+      scrollUpButton.classList.add('hidden');
+    }
+  });
+
+  scrollUpButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
