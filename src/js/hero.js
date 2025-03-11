2f5485179ea3d03958acf7a59c81edde081ee381
DmytroVorobiov1212 <116897771+DmytroVorobiov1212@users.noreply.github.com>
2025-03-11 19:13:33 +0100

    update change theme (#19)

--- src/js/hero.js
+++ src/js/hero.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgMenu = document.querySelector('[data-theme-burg-menu]');
     const themeButtons = document.querySelectorAll('[data-theme-btn]');
     const themeTexts = document.querySelectorAll('[data-theme-text]');
+    const themeTextsSpan = document.querySelectorAll('[data-theme-text-span]');
+    const themeSvgFill = document.querySelectorAll('[data-theme-text-svg]');
+    const themeSwiperBg = document.querySelectorAll('[data-theme-swiper-active]');
     const themeRedBtn = document.querySelector('[data-theme-red]');
     const themeGreenBtn = document.querySelector('[data-theme-green]');
     const themeBlueBtn = document.querySelector('[data-theme-blue]');
     const themeOrangeBtn = document.querySelector('[data-theme-orange]');
-    const themeDarkGreenBtn = document.querySelector('[data-theme_dark-green]');
+    const themeDarkGreenBtn = document.querySelector('[data-theme-dark-green]');
     const themeYellowBtn = document.querySelector('[data-theme-yellow]');
 
     let savedTheme = localStorage.getItem('selectedTheme') || 'theme-red';
@@ -18,11 +21,24 @@ document.addEventListener('DOMContentLoaded', () => {
         burgMenu.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow');
         themeButtons.forEach(button => button.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow'));
         themeTexts.forEach(text => text.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow'));
+        themeTextsSpan.forEach(text => text.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow'));
+        themeSvgFill.forEach(text => text.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow'));
+        themeSwiperBg.forEach(text => text.classList.remove('theme-red', 'theme-green', 'theme-blue', 'theme-orange', 'theme-dark-green', 'theme-yellow'));
 
         hero.classList.add(theme);
         burgMenu.classList.add(theme);
         themeButtons.forEach(button => button.classList.add(theme));
         themeTexts.forEach(text => text.classList.add(theme));
+        themeTextsSpan.forEach(text => text.classList.add(theme));
+        themeSvgFill.forEach(text => text.classList.add(theme));
+        themeSwiperBg.forEach(text => text.classList.add(theme));
+
+        themeRedBtn.classList.toggle('active-btn-change', theme === 'theme-red');
+        themeGreenBtn.classList.toggle('active-btn-change', theme === 'theme-green');
+        themeBlueBtn.classList.toggle('active-btn-change', theme === 'theme-blue');
+        themeOrangeBtn.classList.toggle('active-btn-change', theme === 'theme-orange');
+        themeDarkGreenBtn.classList.toggle('active-btn-change', theme === 'theme-dark-green');
+        themeYellowBtn.classList.toggle('active-btn-change', theme === 'theme-yellow');
 
         localStorage.setItem('selectedTheme', theme);
     }
@@ -33,4 +49,10 @@ document.addEventListener('DOMContentLoaded', () => {
     themeOrangeBtn.addEventListener('click', () => applyTheme('theme-orange'));
     themeDarkGreenBtn.addEventListener('click', () => applyTheme('theme-dark-green'));
     themeYellowBtn.addEventListener('click', () => applyTheme('theme-yellow'));
+});
+
+AOS.init({
+    duration: 1300,
+    once: false,
+    mirror: true,
 });
